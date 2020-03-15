ac8bd31b6ee0fd8ec4725d8184fec1079336e622
Ludmila Korchnoy <lkorchnoy@yahoo.com>
2020-03-15 00:16:13 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -3,7 +3,7 @@ class Show < ActiveRecord::Base
   def highest_rating
     #Show.maximum(:rating)
     #Show.where("rating > ?", 10)
-    Show.where(("highest_rating=10")
+    Show.where("highest_rating=10")
  end
 
  def most_popular_show
