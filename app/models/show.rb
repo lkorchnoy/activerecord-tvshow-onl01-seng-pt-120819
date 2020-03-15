f4c1b8070260ba578b80376079d57b4494156a43
Ludmila Korchnoy <lkorchnoy@yahoo.com>
2020-03-15 00:08:33 +0000

    Automatically backed up by Learn

---
+++ app/models/show.rb
@@ -0,0 +1,33 @@
+class Show < ActiveRecord::Base 
+  
+  def highest_rating
+    #Show.maximum(:rating)
+    #Show.where("rating > ?", 10)
+    Show.where(("highest_rating=10")
+ end
+
+ def most_popular_show
+   Show.order(rating: :desc).first
+ end
+  
+  def lowest_rating
+    Show.minimum("rating")
+  end
+ 
+  def least_popular_show
+    Show.order(rating: :asc).first
+  end
+ 
+  def ratings_sum
+    Show.sum(:rating)
+  end
+ 
+   def popular_shows
+     Show.where("rating > ?", 5)
+   end
+ 
+   def shows_by_alphabetical_order
+     Show.order(name: :asc)
+   end
+
+end 
